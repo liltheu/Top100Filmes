a7312c09933070598b4360714b23f949c2209e53
liltheu <50384574+liltheu@users.noreply.github.com>
2021-04-20 17:08:04 -0300

    Web Scraping Python

---
+++ bs.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests, json
+
+def writeToJSONFile(path, fileName, data):
+    filePathNameWExt = './' + path + '/' + fileName + '.json'
+    with open(filePathNameWExt, 'w') as fp:
+        json.dump(data, fp)
+path = './'
+fileName = 'dados'
+
+url = requests.get('https://www.imdb.com/search/title/?count=100&groups=top_1000&sort=user_rating')
+
+soup = BeautifulSoup(url.content, 'html.parser')
+
+filmes = soup.findAll('div', {'class':'lister-item mode-advanced'})
+
+dados = []
+for filme in filmes:
+    titulo = (filme.h3.a.text)
+    ano = (filme.find('span', {'class':'lister-item-year text-muted unbold'}).text[1:5])
+    nota = (filme.find('div', {'class':'inline-block ratings-imdb-rating'})['data-value'])
+
+    dados.append({
+      'titulo': titulo,
+      'ano': ano,
+      'nota': nota
+    })
+writeToJSONFile(path, fileName, dados)
